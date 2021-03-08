db2a4ee3b95619776375e761350055bb347e74e8
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-03-07 22:53:07 -0300

    adds routes for the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { RoomListComponent } from './room-list/room-list.component';
+import { UptateRoomComponent } from './uptate-room/uptate-room.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'rooms', pathMatch: 'full' },
+  { path: 'rooms', component: RoomListComponent },
+  { path: 'add', component: CreateRoomComponent },
+  { path: 'update/:id', component: UptateRoomComponent },
+  { path: 'details/:id', component: RoomDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
